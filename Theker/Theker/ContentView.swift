d5f67df2230c683b716b33b0d8190b6c91cb6e3d
SarahAlmutairi <76258593+SarahAlmutairi@users.noreply.github.com>
2020-12-25 15:15:54 +0300

    cw-1 done

---
+++ Theker/Theker/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Theker
+//
+//  Created by Sarah Almutairi on 24/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("bg")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image ("top")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                
+                Text("الحمدلله رب العالمين")
+                
+                Spacer()
+                
+                Image("bottom")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Theker/Theker/ThekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThekerApp.swift
+//  Theker
+//
+//  Created by Sam Al-Ahmed on 24/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ThekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
